67fca2d0858493820a923259c3e01183fa766236
Benny Tamburrino <bennytamburrino@gmail.com>
2022-01-27 16:21:03 -0800

    Adjusted Experience tab and updated resume

--- src/websitePages/ExperienceComp.js
+++ src/websitePages/ExperienceComp.js
@@ -30,70 +30,6 @@ const ExperienceComp = () => {
 
   return (
     <div className=" px-10">
-      <div className="md:flex items-center border-b border-sky-800 pb-12 pt-10 ">
-        <div className="w-full">
-          <a
-            className="w-full"
-            href="https://directdental.com/ "
-            target="_blank"
-          >
-            <img
-              src={DirectDentalLogo}
-              alt="DirectDentalLogo"
-              className="sm:w-1/3 md:w-96 "
-            />
-          </a>
-          <p className="Exp-info pt-5 text-left w-96">
-            Contracted for a total site overhaul and mobile app development.
-            <br />
-            More info{" "}
-            <a className="text-sky-500" href={resume} target="_blank">
-              Here.
-            </a>
-          </p>
-        </div>
-
-        <div className="Exp-titles flex items-start justify-between w-full">
-          <div className="pl-3 w-full pt-3">
-            <p className="text-xl font-medium leading-5 text-gray-800">
-              Contracted Software Developer
-            </p>
-            <p className="text-md leading-normal pt-2 text-gray-500">
-              [2021-Present]{" "}
-            </p>
-          </div>
-        </div>
-        <div className="md:w-1/4">
-          <button
-            className={
-              showTools.DD
-                ? "text-5xl leading-normal text-sky-800 -rotate-45 duration-300 hover:scale-125"
-                : "text-5xl leading-normal text-sky-800 duration-300 hover:scale-125"
-            }
-            onClick={() => toolsHandler(0)}
-          >
-            +
-          </button>
-        </div>
-      </div>
-
-      <div
-        className={
-          showTools.DD
-            ? "Exp-div md:flex items-center pb-6 pt-10 justify-evenly duration-300"
-            : "hidden duration-300"
-        }
-      >
-        <i className="Exp-icons devicon-react-original colored" />
-        <i className="Exp-icons devicon-rails-plain colored" />
-        <i className="Exp-icons devicon-javascript-plain colored " />
-        <i className="Exp-icons devicon-bootstrap-plain colored" />
-        <i className="Exp-icons devicon-typescript-plain colored" />
-        <i className="Exp-icons devicon-docker-plain colored" />
-        <i className="Exp-icons devicon-html5-plain colored" />
-        <i className="Exp-icons devicon-css3-plain colored" />
-      </div>
-
       <div className="md:flex items-center border-b border-sky-800 pb-12 pt-20">
         <div className="w-full">
           <a
@@ -156,6 +92,70 @@ const ExperienceComp = () => {
         <i className="Exp-icons devicon-css3-plain colored" />
         <i className="Exp-icons devicon-bootstrap-plain colored" />
         <i className="Exp-icons devicon-docker-plain colored" />
+      </div>
+
+      <div className="md:flex items-center border-b border-sky-800 pb-12 pt-10 ">
+        <div className="w-full">
+          <a
+            className="w-full"
+            href="https://directdental.com/ "
+            target="_blank"
+          >
+            <img
+              src={DirectDentalLogo}
+              alt="DirectDentalLogo"
+              className="sm:w-1/3 md:w-96 "
+            />
+          </a>
+          <p className="Exp-info pt-5 text-left w-96">
+            Contracted for a total site overhaul and mobile app development.
+            <br />
+            More info{" "}
+            <a className="text-sky-500" href={resume} target="_blank">
+              Here.
+            </a>
+          </p>
+        </div>
+
+        <div className="Exp-titles flex items-start justify-between w-full">
+          <div className="pl-3 w-full pt-3">
+            <p className="text-xl font-medium leading-5 text-gray-800">
+              Contracted Software Developer
+            </p>
+            <p className="text-md leading-normal pt-2 text-gray-500">
+              [2021-Present]{" "}
+            </p>
+          </div>
+        </div>
+        <div className="md:w-1/4">
+          <button
+            className={
+              showTools.DD
+                ? "text-5xl leading-normal text-sky-800 -rotate-45 duration-300 hover:scale-125"
+                : "text-5xl leading-normal text-sky-800 duration-300 hover:scale-125"
+            }
+            onClick={() => toolsHandler(0)}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
+      <div
+        className={
+          showTools.DD
+            ? "Exp-div md:flex items-center pb-6 pt-10 justify-evenly duration-300"
+            : "hidden duration-300"
+        }
+      >
+        <i className="Exp-icons devicon-react-original colored" />
+        <i className="Exp-icons devicon-rails-plain colored" />
+        <i className="Exp-icons devicon-javascript-plain colored " />
+        <i className="Exp-icons devicon-bootstrap-plain colored" />
+        <i className="Exp-icons devicon-typescript-plain colored" />
+        <i className="Exp-icons devicon-docker-plain colored" />
+        <i className="Exp-icons devicon-html5-plain colored" />
+        <i className="Exp-icons devicon-css3-plain colored" />
       </div>
 
       <div className="md:flex items-center border-b border-sky-800 pb-12 pt-20">
